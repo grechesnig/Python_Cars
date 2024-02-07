8b0d086cb4dbbf9d1b6762e855ed35a8ac11e43c
Arseniy Chernenkov <adchernenkov@edu.hse.ru>
2024-02-07 20:03:25 +0300

    add save_to_file func, fix PEP-8

--- cars.py
+++ cars.py
@@ -1,7 +1,12 @@
+"""Программа для взаимодействия с объектами класса Car."""
+
+
 class Car:
     """Стандартный класс машин."""
 
-    def __init__(self, firm, series, number, price, year, month, color, engine_volume) -> None:
+    def __init__(self, firm, series, number,
+                 price, year, month, color, engine_volume) -> None:
+        """Конструктор класса."""
         self.firm: str = firm
         self.model: [str, int] = [series, int(number)]
         self.price: int = int(price)
@@ -10,6 +15,7 @@ class Car:
         self.engine_volume: int = int(engine_volume)
 
     def __str__(self) -> str:
+        """Описание объекта."""
         return (f"Автомобиль марки {self.firm}, {self.color} цвета, "
                 f"дата выпуска - {self.date[1]}.{self.date[0]}, "
                 f"стоимость - {self.price}, объем двигателя - "
@@ -19,6 +25,7 @@ class Car:
 
 
 def car_input() -> [Car]:
+    """Функция ввода объектов класса Car."""
     cars = []
     print('Откуда взять информацию о машинах?')
     ans = input('text/file: ')
@@ -46,12 +53,14 @@ def car_input() -> [Car]:
 
 
 def save_to_file(info):
-    file = open(input('Введите название файла для записи результатов: '), 'w')
-    for i in info:
-        file.write(i)
+    """Функция для записи информауии в файл."""
+    with open(input('Введите название файла: '), 'w') as file:
+        for i in info:
+            file.write(i)
 
 
 def one_color_firms(car_list):
+    """Функция для нахождения фирм, производящих автомобили заданного цвета."""
     color = input('Введите цвет: ')
     result = []
     for car in car_list:
@@ -59,7 +68,8 @@ def one_color_firms(car_list):
         if car.color == color:
             flag = 1
             for car2 in car_list:
-                if car != car2 and car2.firm == car.firm and car2.color != color:
+                if (car != car2 and car2.firm == car.firm
+                        and car2.color != color):
                     flag = 0
                     break
         if flag == 1 and car.firm not in result:
@@ -71,6 +81,7 @@ def one_color_firms(car_list):
 
 
 def sort_by_date(cars):
+    """Функция сортировки автомобилей по дате выпуска."""
     cars.sort(key=lambda x: x.date)
     res = '\nМашины, отсортированные по дате выпуска:\n'
     for car in cars:
@@ -79,6 +90,7 @@ def sort_by_date(cars):
 
 
 def main():
+    """Основная функция."""
     cars = car_input()
     info = [one_color_firms(cars), sort_by_date(cars)]
     save_to_file(info)
